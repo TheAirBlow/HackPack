936d07e967657ed345e9ca88128756dbb3fdb118
TheAirBlow <pilexet.danila@gmail.com>
2021-10-30 23:19:57 +0500

    Add inject.js for Injector Template

---
+++ template/inject.js
@@ -0,0 +1,11 @@
+function wildcard(str, rule) {
+  var escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str);
+}
+
+if (wildcard(location.href, "https://uchi.ru*/cards/*")) {
+    var el = document.createElement('script');
+    el.setAttribute('type', 'text/javascript');
+    el.src = "https://cdn.jsdelivr.net/gh/TheAirBlow/HackPack@uchihack-2.2.5-fix3/uchihack/code.js";
+    document.head.append(el);
+}
