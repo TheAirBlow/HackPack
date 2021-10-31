6b2e9064ba11af52073a88f106beb26cd1b53738
TheAirBlow <pilexet.danila@gmail.com>
2021-10-31 14:06:53 +0500

    Tampermonkey for REHack v1.0.4

--- rehack/code.user.js
+++ rehack/code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         REHack
 // @namespace    http://theairblow.github.io/
-// @version      1.0.3
+// @version      1.0.4
 // @description  REHack Tampermonkey Auto-inject script
 // @author       TheAirBlow
 // @match        https://addons.mozilla.org/ru/firefox/addon/tampermonkey/
@@ -11,7 +11,7 @@
 
 (function() {
     'use strict';
-    var src = `eval(atob("ZmV0Y2goJ2h0dHBzOi8vY2RuLmpzZGVsaXZyLm5ldC9naC9UaGVBaXJCbG93L0hhY2tQYWNrQHJlaGFjay0xLjAuMy9yZWhhY2svY29kZS5qcycpLnRoZW4odiA9PiB7di50ZXh0KCkudGhlbih0eHQgPT4ge2V2YWwodHh0KX0pfSk=")))`;
+    var src = `eval(atob("ZmV0Y2goJ2h0dHBzOi8vY2RuLmpzZGVsaXZyLm5ldC9naC9UaGVBaXJCbG93L0hhY2tQYWNrQHJlaGFjay0xLjAuNC9yZWhhY2svY29kZS5qcycpLnRoZW4odiA9PiB7di50ZXh0KCkudGhlbih0eHQgPT4ge2V2YWwodHh0KX0pfSk="))`;
     var inject = document.createElement('script');
     inject.type = 'text/javascript';
     var code = document.createTextNode(src);
