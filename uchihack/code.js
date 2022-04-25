e2bf88dba7c7fc28f96b83bc247cde67350da308
TheAirBlow <pilexet.danila@gmail.com>
2022-04-25 20:32:36 +0500

    Translated code to Russian (v2.3.1)

--- uchihack/code.js
+++ uchihack/code.js
@@ -22,10 +22,10 @@ $('body').ready(() => {
         // ----------------------------------------------------------------------------------
         // Initialization
         globalThis.UchiHack = {};
-        globalThis.UchiHack.version = "v2.3.0";
-        console.warn("%c[UchiHack]" + `%c Version ${globalThis.UchiHack.version} (Made by TheAirBlow)`, style1, style5);
-        console.warn("%c[UchiHack]" + "%c Originally uploaded on Github Gists, then to a Github repo ", style1, style5);
-        console.warn("%c[UchiHack]" + "%c Link: https://github.com/theairblow/hackpack/ ", style1, style5);
+        globalThis.UchiHack.version = "v2.3.1";
+        console.warn("%c[UchiHack]" + `%c Версия ${globalThis.UchiHack.version} (Сделано TheAirBlow)`, style1, style5);
+        console.warn("%c[UchiHack]" + "%c Сначало было на Github Gists, потом перенесено на Github", style1, style5);
+        console.warn("%c[UchiHack]" + "%c Ссылка: https://github.com/theairblow/hackpack/ ", style1, style5);
 
         count = 0;
         isOld = false;
@@ -33,48 +33,48 @@ $('body').ready(() => {
         // ----------------------------------------------------------------------------------
         // More checks
         if (typeof Card === 'undefined') {
-            console.error("%c[UchiHack]" + "%c \"Card\" check failed! ", style1, style4);
+            console.error("%c[UchiHack]" + "%c \"Card\" не существует! ", style1, style4);
             return;
         }
 
         if (typeof Card.Player === 'undefined') {
-            console.error("%c[UchiHack]" + "%c \"Card.Player\" check failed! ", style1, style4);
+            console.error("%c[UchiHack]" + "%c \"Card.Player\" не существует! ", style1, style4);
             return;
         }
 
         if (typeof Card.Player._emitSignal === 'undefined') {
-            console.log("%c[UchiHack]" + "%c \"Card.Player._emitSignal\" not detected, this is an old exercise ", style1, style3);
+            console.log("%c[UchiHack]" + "%c \"Card.Player._emitSignal\" не найден, это старое задание ", style1, style3);
             isOld = true;
         }
 
         // ----------------------------------------------------------------------------------
-        // Send an API request to "events"
+        // Отправить API реквест к "events"
         function send_event(a, b) {
-            console.log("%c[UchiHack]" + "%c Sending an API request to \"events\"... ", style1, style2);
-            console.group("Request information");
-            console.log("Event: ", a);
-            console.log("Data: ", b);
+            console.log("%c[UchiHack]" + "%c Отправляем API реквест к \"events\"... ", style1, style2);
+            console.group("Информация о реквесте");
+            console.log("Событие: ", a);
+            console.log("Данные: ", b);
             console.groupEnd();
-            if (isOld) Card.Player.__score.tutor._sys_event(a, b); // Old exercise method
-                                                                   // Has a major downside: Cannot be used if you break the exercise,
-                                                                   // which means that exercise will be broken forever!
-            else Card.Player._emitSignal(a, b);                    // New exercise method
+            if (isOld) Card.Player.__score.tutor._sys_event(a, b); // Старый метод. Имеет большой минус,
+                                                                   // он не может быть использован если
+                                                                   // задание сломалось.
+            else Card.Player._emitSignal(a, b);                    // Метод для новый заданий
         }
 
         // ----------------------------------------------------------------------------------
-        // Report exercise as solved
+        // Пометить карточку решенной
         function report_solve() {
-            console.log("%c[UchiHack]" + "%c Sending \"$lesson_finish\"... ", style1, style2);
+            console.log("%c[UchiHack]" + "%c Отправляем \"$lesson_finish\"... ", style1, style2);
             send_event("$lesson_finish");
             reload_on_sent();
         }
 
         // ----------------------------------------------------------------------------------
-        // Get score JSON
+        // Получить Score JSON
         function get_score_json() {
-            console.log("%c[UchiHack]" + "%c Getting score JSON... ", style1, style2);
+            console.log("%c[UchiHack]" + "%c Получаем Score JSON... ", style1, style2);
             var n = {};
-            Card.Player.__score.save(n); // Works with old and new exercises
+            Card.Player.__score.save(n); // Работает для старых и новых
             console.group("Score JSON");
             console.log("Data: ", n);
             console.groupEnd();
@@ -84,40 +84,40 @@ $('body').ready(() => {
         // ----------------------------------------------------------------------------------
         // Solve current exercise
         function solve_current() {
-            console.log("%c[UchiHack]" + "%c Solving current exercise... ", style1, style2);
-            // Some black magic
-            if (Card.Player.__score.current + 1 <= Card.Player.__score.total) // Add 1 to "__score.current"
-                Card.Player.__score.current++;                                // if "__score.total" allows us to
-            // More black magic
-            if (Card.Player.__score._index + 2 <= Card.Player.__score.total)  // Add 2 to "__score._index"
-                Card.Player.__score._index += 2;                              // if "__score.total" allows us to
-            else Card.Player.__score._index--;                                // Else decrement it
-            // Mark current exercise as successfully finished
+            console.log("%c[UchiHack]" + "%c Решаем текущее задание... ", style1, style2);
+            // Черная магия
+            if (Card.Player.__score.current + 1 <= Card.Player.__score.total) // Добавим один к "__score.current"
+                Card.Player.__score.current++;                                // если "__score.total" дает нам
+            // Ешё больше черной магии
+            if (Card.Player.__score._index + 2 <= Card.Player.__score.total)  // Добавим два к "__score._index"
+                Card.Player.__score._index += 2;                              // если "__score.total" дает нам
+            else Card.Player.__score._index--;                                // это сделать, если нет то вычитаем
+            // Обозначить текущее задание решенным
             send_event("beads_exercise_finish_succ", {
                 "amount": Card.Player.__score.current,
                 "total": Card.Player.__score.total
             });
-            // Report current "__score"
-            if (isOld) send_event("$store", get_score_json()); // Old exercise method
-            else send_event("$store", {                        // New exercise method,
-                "json": JSON.stringify(get_score_json())       // just a small difference
+            // Отправляем текущий "__score"
+            if (isOld) send_event("$store", get_score_json()); // Метод для старых заданий
+            else send_event("$store", {                        // Метод для новых заданий,
+                "json": JSON.stringify(get_score_json())       // только небольное отличие
             });
         }
 
         // ----------------------------------------------------------------------------------
-        // Enable auto-solve
+        // Включить автоматическое решение
         function solve_all() {
-            console.log("%c[UchiHack]" + "%c Auto-solve enabled! ", style1, style3);
+            console.log("%c[UchiHack]" + "%c Автоматическое решение включено! ", style1, style3);
             sessionStorage.setItem('doSolve', 'true');
             solve_current();
-            if (Card.Player.__score.current >= Card.Player.__score.total) // Report exercise as solved
-                report_solve();                                           // If it's finished
+            if (Card.Player.__score.current >= Card.Player.__score.total) // Помечаем карточку решенной
+                report_solve();                                           // если она решено xD
 
             reload_on_sent();
         }
 
         // ----------------------------------------------------------------------------------
-        // Wait for ajax request to be sent
+        // Ждем пока AJAX реквест отправится
         function test_count() {
             if (count >= 1) {
                 location.reload(false);
@@ -128,50 +128,49 @@ $('body').ready(() => {
         }
 
         // ----------------------------------------------------------------------------------
-        // Reload the page when ajax request is sent
+        // Перезагрузить страницу когда AJAX реквест отпарвится
         function reload_on_sent() {
             setTimeout(function() { test_count(); }, 50);
             $(document).ajaxStop(function () {
                 count++;
-                console.log("Ajax: Request sent")
             });
         }
 
         // ----------------------------------------------------------------------------------
-        // Status
+        // Статус
         color = "green";
 
         if (sessionStorage.getItem('doSolve') === 'true' && sessionStorage.getItem('solved') !== 'true') {
             color = "orange";
-            status = "Solving";
+            status = "Решаем";
         }
         else if (sessionStorage.getItem('solved') === 'true')
-            status = "Solved";
+            status = "Решено";
         else if (isOld)
-            status = "Old exercises support";
-        else status = "Ready";
+            status = "Поддержка старых заданий";
+        else status = "Готов";
 
         // ----------------------------------------------------------------------------------
-        // Buttons
+        // Кнопки
         if (globalThis.UchiHack.status !== "Решаем") {
-            // Solve all
+            // Решить всю карточку
             var root = $("<div>").css("margin", "-20px auto 20px").css("width", "1024px");
             var obj1 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff")
                 .css("border-radius", "20px").css("padding", "0 8px 0 8px").css("width", "max-content")
-                .append($("<a>").append($("<span>").css("cursor", "pointer").text("Solve all exercises")).on("click", function() {
+                .append($("<a>").append($("<span>").css("cursor", "pointer").text("Решить карточку")).on("click", function() {
                     solve_all();
                 }));
 
-            // Status and version
+            // Статус и версия
             var obj3 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff")
                 .css("border-radius", "20px").css("top", "-25px").css("padding", "0 8px 0 8px").css("width", "max-content").css("margin", "auto")
                 .append($("<a>").append(`<a style=\"cursor: pointer;\" href="https://github.com/TheAirBlow/HackPack/tree/main/uchihack" target="_blank">UchiHack ${globalThis.UchiHack.version}</a>`)
-                .append($("<span style=\"color: black;\"> | Status: </span>")).append($(`<span style=\"color: ${color};\">${status}</span>`)));
+                .append($("<span style=\"color: black;\"> | Статус: </span>")).append($(`<span style=\"color: ${color};\">${status}</span>`)));
 
-            // Solve current
+            // Решить текущее задание
             var obj2 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff").css("left", "83%")
                 .css("border-radius", "20px").css("top", "-50px").css("padding", "0 6px 0 8px").css("width", "max-content")
-                .append($("<a>").append($("<span>").css("cursor", "pointer").text("Solve current exercise")).on("click", function() {
+                .append($("<a>").append($("<span>").css("cursor", "pointer").text("Решить задание")).on("click", function() {
                     solve_current();
                     reload_on_sent();
                 }));
@@ -183,11 +182,11 @@ $('body').ready(() => {
         }
 
         // ----------------------------------------------------------------------------------
-        // Auto-solve
+        // Автоматическое решение
         if (sessionStorage.getItem('doSolve') === 'true') {
-            console.log("%c[UchiHack]" + "%c Continuing auto-solve... ", style1, style2);
+            console.log("%c[UchiHack]" + "%c Продолжаем решение карточки... ", style1, style2);
             if (sessionStorage.getItem('solved') === 'true') {
-                console.log("%c[UchiHack]" + "%c Exercise was solved! ", style1, style3);
+                console.log("%c[UchiHack]" + "%c Карточка успешно решена! ", style1, style3);
                 sessionStorage.setItem('doSolve', 'false');
                 sessionStorage.setItem('solved', 'false');
             } else if (Card.Player.__score.current === Card.Player.__score.total) {
@@ -203,7 +202,7 @@ $('body').ready(() => {
             }
         }
 
-        console.log("%c[UchiHack]" + "%c Script has finished it's work! ", style1, style3);
+        console.log("%c[UchiHack]" + "%c Скрипт закончил свою работу! ", style1, style3);
     };
 
     setTimeout(data, 50);
